e60319fa6b4bc3b35069a0a127b67ff8302b0e84
Jesse-Tingle <jtingle0@gmail.com>
2020-03-16 21:21:46 -0400

    ran new migration to update title of database

--- users/users-model.js
+++ users/users-model.js
@@ -1,6 +1,9 @@
-const db = require("../database/config");
+const bcrypt = require("bcrypt");
+const db = require("../data/config");
 
 async function add(user) {
+	user.password = await bcrypt.hash(user.password, 10);
+
 	const [id] = await db("users").insert(user);
 	return findById(id);
 }
